2a1e8da5cdedce51cdf764a7f9e6764ceecf3d68
BinZ <sikatunar@gmail.com>
2024-12-06 16:47:26 +0800

    init

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config: any) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-const nextConfig: NextConfig = {
-  /* config options here */
+    return config;
+  },
 };
 
 export default nextConfig;
