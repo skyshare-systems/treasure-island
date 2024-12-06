5f8e6ca199e4f101864cd9ae64164d58121ff8f2
BinZ <sikatunar@gmail.com>
2024-12-06 18:51:53 +0800

    add icons

---
+++ apps/web/components/icon/arrow-up-right.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ArrowUpRight = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+    >
+      <path
+        d="M9.91667 4.0835L3.5 10.5002"
+        stroke="#141B34"
+        stroke-width="0.875"
+        stroke-linecap="round"
+      />
+      <path
+        d="M6.41699 3.57673C6.41699 3.57673 9.7032 3.29971 10.202 3.79841C10.7006 4.29711 10.4236 7.58335 10.4236 7.58335"
+        stroke="#141B34"
+        stroke-width="0.875"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowUpRight;
