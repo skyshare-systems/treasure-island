7025fb1f80239a78be4eaeda9a466f06134c0f30
BinZ <sikatunar@gmail.com>
2024-12-08 17:35:08 +0800

    add icon

---
+++ apps/web/components/icon/x-icon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const XIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+    >
+      <path
+        d="M11.0837 2.9165L2.91699 11.0832M2.91699 2.9165L11.0837 11.0832"
+        stroke="black"
+        stroke-width="0.875"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default XIcon;
