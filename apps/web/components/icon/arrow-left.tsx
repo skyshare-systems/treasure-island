54413b42f49737e8f2c89c0fad8576b8b35b1edd
BinZ <sikatunar@gmail.com>
2024-12-12 16:58:48 +0800

    Create arrow-left.tsx

---
+++ apps/web/components/icon/arrow-left.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ArrowLeft = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="15"
+      viewBox="0 0 14 15"
+      fill="none"
+    >
+      <path
+        d="M8.75 4C8.75 4 5.25001 6.57769 5.25 7.5C5.24999 8.42237 8.75 11 8.75 11"
+        stroke="black"
+        stroke-width="0.875"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowLeft;
