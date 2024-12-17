9eb5e3dc0a56eaf347ae598348fbc351823ee987
BinZ <sikatunar@gmail.com>
2024-12-17 13:17:40 +0800

    modify

--- apps/web/views/marketplace/dashboard.tsx
+++ apps/web/views/marketplace/dashboard.tsx
@@ -1,19 +1,11 @@
-import { cn } from "@/lib/utils";
-import { fredoka } from "@/public/fonts";
 import React, { useEffect } from "react";
-import Image from "next/image";
 import AttackModal from "./attack-modal";
-import { useAttackModal } from "@/lib/store/attack-modal-store";
 import { useDashboardModal } from "@/lib/store/dashboard-modal-store";
-import ArrowLeft from "@/components/icon/arrow-left";
 import LandList from "./landlist";
 import SelectedLand from "./selected-land";
+import SwapView from "../swap/swap-view";
 
 const Dashboard = () => {
-  const { isShowAttackModal, setIsShowAttackModal } = useAttackModal(
-    (state) => state
-  );
-
   const { dashboardCount } = useDashboardModal((state) => state);
 
   useEffect(() => {
@@ -24,6 +16,7 @@ const Dashboard = () => {
   return (
     <>
       <LandList />
+      <SwapView />
       <SelectedLand />
       <AttackModal />
     </>
--- apps/web/views/marketplace/page.tsx
+++ apps/web/views/marketplace/page.tsx
@@ -5,6 +5,7 @@ import { fredoka } from "@/public/fonts";
 import React, { useState } from "react";
 import Dashboard from "./dashboard";
 import { useDashboardModal } from "@/lib/store/dashboard-modal-store";
+import Swap from "../swap/page";
 
 const Marketplace = () => {
   const { dashboardCount, setDashboardCount } = useDashboardModal(
@@ -13,24 +14,27 @@ const Marketplace = () => {
   return (
     <div className="fixed flex flex-col items-end justify-end md:right-8 gap-2 md:bottom-8 bottom-4 right-0 px-4 z-[3]">
       <Dashboard />
+      <div className="flex items-center gap-4">
+        <Swap />
 
-      <button
-        onClick={() =>
-          dashboardCount >= 1 ? setDashboardCount(0) : setDashboardCount(1)
-        }
-        className="flex items-center gap-1 p-3 border-2 border-black bg-white rounded-xl z-[3]"
-      >
-        {dashboardCount >= 1 ? (
-          <XIcon />
-        ) : (
-          <>
-            <DashboardIcon />
-            <h1 className={cn(fredoka.className, "ty-title text-black")}>
-              Marketplace
-            </h1>
-          </>
-        )}
-      </button>
+        <button
+          onClick={() =>
+            dashboardCount === 1 ? setDashboardCount(0) : setDashboardCount(1)
+          }
+          className="flex items-center gap-1 p-3 border-2 border-black bg-white rounded-xl z-[3]"
+        >
+          {dashboardCount === 1 ? (
+            <XIcon />
+          ) : (
+            <>
+              <DashboardIcon />
+              <h1 className={cn(fredoka.className, "ty-title text-black")}>
+                Marketplace
+              </h1>
+            </>
+          )}
+        </button>
+      </div>
     </div>
   );
 };
