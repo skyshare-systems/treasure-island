d5ef081cbcb827b9a60bcd0128b1bfd3e709afa3
Binz <sikatunar@gmail.com>
2024-12-26 19:49:58 +0800

    Create selected-land-store.ts

---
+++ apps/web/lib/store/selected-land-store.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+//* STATE NG ZUSTAND
+export interface ISelectedLand {
+  item: SelectedItem | null;
+  setItem: (item: SelectedItem) => void;
+}
+
+interface SelectedItem {
+  name: string;
+  image: string;
+  tag: string;
+  sui: number | string;
+  token: number | string;
+  percentage: number | string;
+}
+
+export const useSelectedLand = create<ISelectedLand>()(
+  persist(
+    (set) => ({
+      item: null,
+      setItem: (item) => set({ item }),
+    }),
+    {
+      name: "selected-land-store",
+    }
+  )
+);
