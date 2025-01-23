8cd5e53f1f0be73ab855df712249f15ea04ce97b
BinZ <sikatunar@gmail.com>
2025-01-23 13:50:57 +0800

    add SEO

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,10 +2,90 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { fredoka } from "@/public/fonts";
 import AttackModal from "@/views/marketplace/attack-modal";
+import {
+  SITE_DESCRIPTION,
+  SITE_NAME,
+  SITE_TITLE,
+  SITE_URL,
+} from "@/config/site";
 
 export const metadata: Metadata = {
-  title: "Treasury Island",
-  description: "Treasury Island",
+  metadataBase: new URL(SITE_URL),
+  title: {
+    default: SITE_TITLE,
+    template: `%s | ${SITE_TITLE}`,
+  },
+  description: SITE_DESCRIPTION,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  manifest: "/favicon/site.webmanifest",
+  twitter: {
+    card: "summary_large_image",
+    title: SITE_NAME,
+    description: SITE_DESCRIPTION,
+    site: "@treasuryisland",
+    creator: "@treasuryisland",
+    images: [
+      {
+        url: `${SITE_URL}/assets/OG.png`,
+        width: 2400,
+        height: 1260,
+        alt: SITE_DESCRIPTION,
+      },
+    ],
+  },
+  creator: "Treasury Island",
+  openGraph: {
+    url: SITE_URL,
+    type: "website",
+    title: SITE_TITLE,
+    siteName: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    locale: "en-US",
+    images: [
+      {
+        url: `${SITE_URL}/assets/og/og.png`,
+        width: 2400,
+        height: 1260,
+        alt: SITE_DESCRIPTION,
+        type: "image/png",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon/favicon.ico",
+    shortcut: "/favicon/favicon.ico",
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ apps/web/config/site.ts
@@ -0,0 +1,8 @@
+export const SITE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://treasure-island-web.vercel.app/"
+    : "http://localhost:3000";
+
+export const SITE_NAME = "Treasury Island";
+export const SITE_TITLE = "Treasury Island";
+export const SITE_DESCRIPTION = "Treasury Island";
